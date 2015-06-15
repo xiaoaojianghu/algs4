64f350571431d7385eb76546286c5982e735055e
reddit <hellothx@qq.com>
2015-06-15 12:31:19 +0800

    weighted quick-union

--- src/fundmental/Union_find2.java
+++ src/fundmental/Union_find2.java
@@ -1,3 +1,4 @@
+//加权quick-union, 小树总是连到大树上
 package fundmental;
 
 import java.io.BufferedReader;
@@ -5,53 +6,44 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Union_find{
+public class Union_find2{
 	private int[] id;
+	private int[] sz;
 	private int N;
-	public Union_find(int n){
+	public Union_find2(int n){
 		id = new int[n];
-		for(int i = 0; i < n; i++)
+		sz = new int[n];
+		for(int i = 0; i < n; i++){
 			id[i] = i;
+			sz[i] = 1;
+		}
 		N = n;
 	}
-	//最早版本, 同一个分量值相同 quick find
-	public void union1(int p, int q){
-		int ip = find1(p);
-		int iq = find1(q);
-		if(ip == iq)
+	
+	public void union(int p, int q){
+		int rootp = find(p);
+		int rootq = find(q);
+		if(rootp == rootq)
 			return;
-		for(int i = 0; i < id.length; i++){
-			if(id[i] == ip)
-				id[i] = iq;
+		if(sz[rootp] > sz[rootq]){
+			id[rootq] = rootp;
+			sz[rootp] += sz[rootq];
+		} else {
+			id[rootp] = rootq;
+			sz[rootq] += sz[rootp];
 		}
 		N--;
 	}
 
-	public int find1(int p){
-		return id[p];
+	public int find(int p){
+		while(p != id[p])
+			p = id[p];
+		return p;
 	}
 
-	//同一个分量用一棵树表示 quick union
-	public void union2(int p, int q){
-		int rootp = find2(p);
-		int rootq = find2(q);
-		if(rootp == rootq)
-			return;
-		id[rootp] = rootq;
-		N--;
-	}
-
-
-	public int find2(int p){
-		int root = p;
-		while(root != id[root])
-			root = id[root];
-		return root;
-	}
-
-
+	
 	public boolean connected(int p, int q){
-		return id[p] == id[q];
+		return find(p) == find(q);
 	}	
 
 	public int count(){
@@ -60,12 +52,10 @@ public class Union_find{
 
 	public static void main(String [] args) throws IOException{
 //		File f = new File("mediumUF.txt");
-//		File f = new File("tinyUF.txt");
 		File f = new File("largeUF.txt");
-
 		BufferedReader r = new BufferedReader(new FileReader(f));
 		String s = r.readLine();
-		Union_find u = new Union_find(Integer.valueOf(s));
+		Union_find2 u = new Union_find2(Integer.valueOf(s));
 		s = r.readLine();
 		String[] ints;
 		while(s != null){
@@ -73,7 +63,7 @@ public class Union_find{
 			int p = Integer.valueOf(ints[0]);
 			int q = Integer.valueOf(ints[1]);
 			System.out.println( p + " " + q);
-			u.union2(p,q);
+			u.union(p,q);
 			s = r.readLine();
 		}
 		r.close();
